4b7cafe90adfdff47037c20bc520016c4bc890c1
Danilo Silva <silva.danilof@outlook.com>
2022-12-17 09:21:26 -0300

    Adicionando camada de modelo

---
+++ src/main/java/br/com/btg/pedidos/model/ItemPedido.java
@@ -0,0 +1,51 @@
+package br.com.btg.pedidos.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_do_pedido")
+public class ItemPedido {
+
+	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+	private String nome;
+
+	private Integer quantidade;
+	
+	private double preco;
+	
+	public ItemPedido() {
+	}
+	
+	public ItemPedido( String nome, Integer quantidade, double preco) {
+		super();
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+}
