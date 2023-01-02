5139e7151448395bc63d337ada8be2d8661df656
Danilo Silva <silva.danilof@outlook.com>
2023-01-02 14:17:42 -0300

    Alternado projeto para utilizar mongodb

--- src/main/java/br/com/btg/pedidos/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.btg.pedidos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.btg.pedidos.model.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-
-}
