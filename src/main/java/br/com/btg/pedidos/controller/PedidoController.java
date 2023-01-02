491b3ddf167cf4241815cfb3f0bc1398d22ea23e
Danilo Silva <silva.danilof@outlook.com>
2023-01-02 14:40:18 -0300

    Implementando mensageria com rabbitmq

--- src/main/java/br/com/btg/pedidos/controller/PedidoController.java
+++ src/main/java/br/com/btg/pedidos/controller/PedidoController.java
@@ -2,9 +2,12 @@ package br.com.btg.pedidos.controller;
 
 import java.util.Optional;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,19 @@ public class PedidoController {
 
 	@Autowired
 	private PedidoRepository repository;
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@PostMapping("/criarPedido")
+	public Pedido criar(@RequestBody Pedido pedido) {
+		repository.save(pedido);
+		String routingKey = "pedido";
+
+		Pedido pedido1 = new Pedido(pedido.getCodigoPedido(), pedido.getCodigoCliente(), pedido.getItens());
+		rabbitTemplate.convertAndSend(routingKey, pedido1);
+		return pedido;
+	}
 
 	@GetMapping("/pedido/{codigoPedido}")
 	public Optional<Pedido> getPedido(@PathVariable String codigoPedido) {
